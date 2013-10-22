f0b057c7573a4f306692d7e0a71addda59afb41a
Eimis <eimantas.stonys@gmail.com>
2013-10-22 19:30:59 +0300

    Blog

--- blog/settings.py
+++ blog/settings.py
@@ -192,7 +192,7 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default='postgres://eimis:maxmaxkit@localhost/blogdb')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
