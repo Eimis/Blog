95e6411d9dfe0d451a9a54721a427564bd2dc041
Eimis <eimantas.stonys@gmail.com>
2013-10-23 19:49:03 +0300

    Blog

--- ckeditor/ckeditor/config.js
+++ ckeditor/ckeditor/config.js
@@ -7,4 +7,5 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.stylesSet = 'cke:http://http://www.protected-badlands-9627.herokuapp.com/static/cke.js';
 };
