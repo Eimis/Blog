39307272e28f48363d9e003bea1b9fa645efe183
Eimis <eimantas.stonys@gmail.com>
2013-11-05 21:20:02 +0200

    pridejau migration

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -8,13 +8,37 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        pass
+        # Adding model 'Post'
+        db.create_table(u'blog_post', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('description', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+            ('pub_date', self.gf('django.db.models.fields.DateTimeField')()),
+            ('text', self.gf('ckeditor.fields.RichTextField')()),
+            ('link_to_image', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=40, blank=True)),
+            ('display', self.gf('django.db.models.fields.BooleanField')(default=True)),
+        ))
+        db.send_create_signal(u'blog', ['Post'])
+
 
     def backwards(self, orm):
-        pass
+        # Deleting model 'Post'
+        db.delete_table(u'blog_post')
+
 
     models = {
-        
+        u'blog.post': {
+            'Meta': {'ordering': "['pub_date']", 'object_name': 'Post'},
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'display': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'link_to_image': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'pub_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '40', 'blank': 'True'}),
+            'text': ('ckeditor.fields.RichTextField', [], {}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
     }
 
     complete_apps = ['blog']
